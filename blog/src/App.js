18922fb36c292f63d286c9a6ea067657db3c1e1f
cats0713 <cats0713@naver.com>
2021-02-03 14:23:59 +0900

    react

---
+++ blog/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className = 'black_nav'>
+       <div>블로그 테스트</div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
